f243f1fb43cd0f8bd48aeabc470d370700a00d2b
Tejas Patil <66073053+Tejasvp25@users.noreply.github.com>
2021-03-15 12:29:29 +0530

    Get config from Env variables

---
+++ config.py
@@ -0,0 +1,10 @@
+import os  
+
+api_id = int(os.getenv('API_ID'))
+api_hash = os.getenv('API_HASH')
+sudo_chat_id = int(os.getenv('SUDO_CHAT_ID'))
+owner_id = int(os.getenv('OWNER_ID'))
+
+
+# Arq Api, don't make changes here
+ARQ = "https://thearq.tech/"
